4db198125e1dfa18188a630e9bea384031e3c806
Антон <mongoostic@yandex.ru>
2021-01-06 17:13:37 +0800

    Верстка header

--- news-explorer-frontend/src/components/UI/UI.js
+++ news-explorer-frontend/src/components/UI/UI.js
@@ -3,25 +3,52 @@ import { NavLink } from 'react-router-dom';
 import './UI.css';
 
 function UI() {
-  return (
+  return <>
     <header className="header header_unauthorized">
       <h2 className="header__title">NewsExplorer</h2>
       <div className="header__nav">
-        <NavLink exact to="/ui" 
-          className="header__nav-link" 
-          activeClassName="header__nav-link_active"
+        <NavLink exact to="/ui"
+          className="header__nav-link header__nav-link_not-logged-in"
+          activeClassName="header__nav-link-active header__nav-link-active_not-logged-in"
         >
-          <h2 className="header__nav-main">Главная</h2>
+          Главная
         </NavLink>
-        <NavLink exact to="/authorize" 
-          className="header__nav-link" 
-          activeClassName="header__nav-link_active"
+        <NavLink exact to="/authorize"
+          className="header__nav-link header__nav-link_not-logged-in"
+          activeClassName="header__nav-link-active header__nav-link-active_not-logged-in"
         >
-          <button className="header__nav-link-authorize">Авторизоваться</button>
+          <button className="header__nav-link-button-log-in">Авторизоваться</button>
         </NavLink>
       </div>
     </header>
-  );
+
+    <div style={{ height: 10 }}></div>
+
+    <header className="header header_authorized">
+      <h2 className="header__title">NewsExplorer</h2>
+      <div className="header__nav">
+        <NavLink exact to="/ui"
+          className="header__nav-link header__nav-link_logged-in"
+          activeClassName="header__nav-link-active header__nav-link-active_logged-in"
+        >
+          Главная
+        </NavLink>
+        <NavLink exact to="/saved_articles"
+          className="header__nav-link header__nav-link_logged-in"
+          activeClassName="header__nav-link-active header__nav-link-active_logged-in"
+        >
+          Сохраненные статьи
+        </NavLink>
+        <NavLink exact to="/authorize"
+          className="header__nav-link"
+          activeClassName="header__nav-link-active header__nav-link-active_logged-in"
+        >
+          <button className="header__nav-link-logout">Грета</button>
+        </NavLink>
+      </div>
+    </header>
+
+    </>;
 }
 
 export default UI;
