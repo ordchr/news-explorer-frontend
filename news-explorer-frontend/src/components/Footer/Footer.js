f725f9dd34d60419d140d0dfd336ad3fb86171b8
Антон <mongoostic@yandex.ru>
2021-03-01 21:10:18 +0800

    Footer

---
+++ news-explorer-frontend/src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import "./Footer.css";
+import IconGithub from "../../images/icon_github.svg";
+import IconFacebook from "../../images/icon_facebook.svg";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h3 className="footer__copyright">
+        © 2020 Supersite, Powered by News API
+      </h3>
+      <div className="footer__navi">
+        <Link to="/" className="footer__navi-link">
+          Главная
+        </Link>
+        <a href="https://praktikum.yandex.ru" className="footer__navi-link">
+          Яндекс.Практикум
+        </a>
+        <div className="footer__navi-social">
+          <a href="https://github.com" className="footer__navi-link">
+            <img src={IconGithub} alt="github" />
+          </a>
+          <a href="https://facebook.com" className="footer__navi-link">
+            <img src={IconFacebook} alt="facebook" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
