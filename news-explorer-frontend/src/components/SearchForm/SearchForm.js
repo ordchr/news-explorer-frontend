eb6d628aabb737aadb0010826222330ee59decd6
Антон <mongoostic@yandex.ru>
2021-03-08 20:44:26 +0800

    SearchForm верстка под разные разрешения

--- news-explorer-frontend/src/components/SearchForm/SearchForm.js
+++ news-explorer-frontend/src/components/SearchForm/SearchForm.js
@@ -9,9 +9,13 @@ function SearchForm() {
 
       <div className="search-form-head">
         <h1 className="search-form-head__title">Что творится в мире?</h1>
-        <h4 className="search-form-head__caption">Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.</h4>
+        <h4 className="search-form-head__caption">
+          Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.
+        </h4>
         <div className="search-form-input">
-          <input className="search-form-input__search-text" placeholder="Введите тему новости" />
+          <div className="search-form-input__search-text-wrapper">
+            <input className="search-form-input__search-text" placeholder="Введите тему новости" />
+          </div>
           <button className="search-form-input__to-go">Искать</button>
         </div>
       </div>
