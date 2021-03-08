325636c412eb45ab5643cc1f3af0ec86731a50e7
Антон <mongoostic@yandex.ru>
2021-03-09 00:29:55 +0800

    Верстка Footer и доработка других блоков

--- news-explorer-frontend/src/components/Footer/Footer.js
+++ news-explorer-frontend/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 import IconGithub from "../../images/icon_github.svg";
 import IconFacebook from "../../images/icon_facebook.svg";
@@ -7,9 +7,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="footer">
-      <h3 className="footer__copyright">
-        © 2020 Supersite, Powered by News API
-      </h3>
+      <h3 className="footer__copyright">© 2020 Supersite, Powered by News API</h3>
       <div className="footer__navi">
         <Link to="/" className="footer__navi-link">
           Главная
@@ -17,14 +15,14 @@ function Footer() {
         <a href="https://praktikum.yandex.ru" className="footer__navi-link">
           Яндекс.Практикум
         </a>
-        <div className="footer__navi-social">
-          <a href="https://github.com" className="footer__navi-link">
-            <img src={IconGithub} alt="github" />
-          </a>
-          <a href="https://facebook.com" className="footer__navi-link">
-            <img src={IconFacebook} alt="facebook" />
-          </a>
-        </div>
+      </div>
+      <div className="footer__navi-social">
+        <a href="https://github.com" className="footer__navi-social-link">
+          <img src={IconGithub} alt="github" />
+        </a>
+        <a href="https://facebook.com" className="footer__navi-social-link">
+          <img src={IconFacebook} alt="facebook" />
+        </a>
       </div>
     </div>
   );
