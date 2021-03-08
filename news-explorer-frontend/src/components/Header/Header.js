8f354e7f19c41c4b41b1c64d1d2218c98b160aca
Антон <mongoostic@yandex.ru>
2021-03-08 19:32:52 +0800

    Доработка header menu

--- news-explorer-frontend/src/components/Header/Header.js
+++ news-explorer-frontend/src/components/Header/Header.js
@@ -1,62 +1,98 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import iconMenu from "../../images/icon_menu.svg";
+import iconMenuClose from "../../images/icon_menu_close.svg";
 
 function Header({ isLoggedIn, isMainPage }) {
-
   const headerNavLinkCSS = `header__nav-link ${
-    isMainPage
-      ? "header__nav-link_main-page"
-      : "header__nav-link_saved-news-page"
+    isMainPage ? "header__nav-link_main-page" : "header__nav-link_saved-news-page"
   }`;
+
+  const headerIconMenuNavLinkCSS = `header-icon-menu__link ${
+    isMainPage ? "header-icon-menu__link_main-page" : "header-icon-menu__link_saved-news"
+  }`;
+
+  const [iconMenuIsOpen, setIconMenuIsOpen] = React.useState(false);
+
+  function handleIconMenuButtonClick() {
+    setIconMenuIsOpen(!iconMenuIsOpen);
+  }
+
+  const logoutButtonCSS = ``;
 
   return (
     <header
       className={`header 
       ${isMainPage ? "header_main-page" : "header_saved-news-page"}
-      `}
+      header_icon_menu`}
     >
       <h2 className="header__title">NewsExplorer</h2>
       <div className="header__nav">
-        <NavLink
-          exact
-          to="/"
-          className={headerNavLinkCSS}
-          activeClassName="header__nav-link-active"
-        >
+        <NavLink exact to="/" className={headerNavLinkCSS} activeClassName="header__nav-link-active">
           Главная
         </NavLink>
         {isLoggedIn ? (
           <>
-            <NavLink
-              exact
-              to="/saved-news"
-              className={headerNavLinkCSS}
-              activeClassName="header__nav-link-active"
-            >
+            <NavLink exact to="/saved-news" className={headerNavLinkCSS} activeClassName="header__nav-link-active">
               Сохраненные статьи
             </NavLink>
-            <NavLink
-              exact
-              to="/authorize"
-              className="header__nav-link"
-              activeClassName="header__nav-link-active"
-            >
-              <button className="header__nav-link-logout">Грета</button>
+            <NavLink exact to="/authorize" className="header__nav-link" activeClassName="header__nav-link-active">
+              <button className="header__nav-link-logout header__nav-link-logout_basic-menu">Грета</button>
             </NavLink>
           </>
         ) : (
-          <NavLink
-            exact
-            to="/authorize"
-            className={headerNavLinkCSS}
-            activeClassName="header__nav-link-active"
-          >
-            <button className="header__nav-link-button-log-in">
-              Авторизоваться
-            </button>
+          <NavLink exact to="/authorize" className={headerNavLinkCSS} activeClassName="header__nav-link-active">
+            <button className="header__button-log-in header__button-log-in_basic-menu">Авторизоваться</button>
           </NavLink>
         )}
+      </div>
+      <img
+        className="header-icon-menu__button"
+        src={iconMenuIsOpen ? iconMenuClose : iconMenu}
+        alt="icon menu"
+        onClick={handleIconMenuButtonClick}
+      />
+
+      <div
+        className={`header-icon-menu ${iconMenuIsOpen ? "header-icon-menu_opened" : "header-icon-menu_closed"}
+        `}
+      >
+        <div className="header-icon-menu__container header-icon-menu_main-page">
+          <div
+            className={`header-icon-menu__main-line ${
+              isMainPage ? "header-icon-menu__main-line_main-page" : "header-icon-menu__main-line_saved-news"
+            }`}
+          >
+            <h2 className="header__title">NewsExplorer</h2>
+            <img
+              className="header-icon-menu__button"
+              src={iconMenuIsOpen ? iconMenuClose : iconMenu}
+              alt="icon menu"
+              onClick={handleIconMenuButtonClick}
+            />
+          </div>
+          <NavLink exact to="/" className={headerIconMenuNavLinkCSS}>
+            Главная
+          </NavLink>
+
+          {isLoggedIn ? (
+            <>
+              <NavLink exact to="/saved-news" className={headerIconMenuNavLinkCSS}>
+                Сохраненные статьи
+              </NavLink>
+              <NavLink exact to="/logout" className="header__nav-link header__nav-link-logout_icon-menu">
+                <button className="header__nav-link-logout header__nav-link-logout_icon-menu header__nav-link-logout_main-page">
+                  Грета
+                </button>
+              </NavLink>
+            </>
+          ) : (
+            <NavLink exact to="/authorize" className={headerIconMenuNavLinkCSS}>
+              <button className="header__button-log-in header__button-log-in_icon-menu">Авторизоваться</button>
+            </NavLink>
+          )}
+        </div>
       </div>
     </header>
   );
